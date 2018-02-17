e99d8591ed35aa5a1a18678c1ca8f2626c0c7d13
Zack Khan <zack.khan101@gmail.com>
2018-02-17 12:59:11 -0800

    more server setup

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(8080);
